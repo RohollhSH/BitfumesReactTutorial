9fe684074b47dd21e38f83700fad93f319a87a47
RohollahSH <rohollahshahbazi93@gmail.com>
2020-07-26 11:06:33 +0430

    10 ReactJS basics ReCreate Index https://youtu.be/Lbb_xRoFSyo

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// let el = "Hello";
+
+// let el = <div>Hello React</div>;
+
+//another way of importing App
+// const App = require("./App").default;
+
+ReactDOM.render(<App />, document.getElementById('root'));
