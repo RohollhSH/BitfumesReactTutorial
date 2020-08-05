d6e3dda93f39e5bfc8807b590536326021f6c929
RohollahSH <rohollahshahbazi93@gmail.com>
2020-08-06 00:21:41 +0430

    27 ReactJS basics Component Inside Component https://youtu.be/H7N0PlIqlRE

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,58 +1,43 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 
+export default function Images() {
+  const Images = [
+    "https://images.unsplash.com/photo-1595845003613-4e0c4e5cc1a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+    "https://p.bigstockphoto.com/jdVZ6zirQdGjAE2vCmmK_bigstock-A-Telltale-Sign-Of-Summer-Hyd-272112430.jpg",
+  ];
 
-export default function Images () {
-  // const [myInterval, setMyInterval] = useState(null);
+  // console.log(
+  //   Images.map((image) => {
+  //     return (
+  //       <div>
+  //         <img src={image} width="150" />
+  //       </div>
+  //     );
+  //   })
+  // );
 
-  useEffect(()=>{
-    console.log("Images Comp Mounted");
-    const interval = setInterval(()=>{
-      console.log("Hello")
-    }, 1000);
-
-    //this returns when component unMounts
-    return () => {
-      // console.log("Images ")
-      clearInterval(interval);
-    }
-  }, []);
-
-  return (
-    <img
-      src="https://images.unsplash.com/photo-1595845003613-4e0c4e5cc1a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-      alt=""/>
-  )
-}
-
-/*
-export default class Images extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {interval: null}
-  }
-
-  componentDidMount() {
-    console.log("Images Comp Mounted");
-    this.setState({
-      interval: setInterval(() => { console.log("Hello") }, 1000)
+  function ShowImage() {
+    return Images.map((image) => {
+      return (
+        <div>
+          <img src={image} width="150" />
+        </div>
+      );
     })
   }
 
-  componentDidUpdate() {
-    console.log("App Updated");
-  }
-
-  componentWillUnmount() {
-    console.log("Images Comp Unmounted");
-    clearInterval(this.state.interval);
-  }
-
-  render() {
-    return (
-      <img
-        src="https://images.unsplash.com/photo-1595845003613-4e0c4e5cc1a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-        alt=""/>
-    );
-  }
-}*/
+  return (
+    <section>
+      <div className="flex justify-center">
+        <ShowImage />
+        {/* {Images.map((image) => {
+          return (
+            <div>
+              <img src={image} width="150" />
+            </div>
+          );
+        })} */}
+      </div>
+    </section>
+  );
+}
