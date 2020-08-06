67a533b17bdceba1640aded8d4e4a355d7548538
RohollahSH <rohollahshahbazi93@gmail.com>
2020-08-06 16:08:49 +0430

    29 ReactJS basics Input OnChange Event https://youtu.be/4uDKxoBJCBs

--- src/components/Images.js
+++ src/components/Images.js
@@ -3,8 +3,10 @@ import React, { useState } from "react";
 export default function Images() {
   const [Images, setImages] = useState([
     "https://images.unsplash.com/photo-1595845003613-4e0c4e5cc1a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
-    "https://p.bigstockphoto.com/jdVZ6zirQdGjAE2vCmmK_bigstock-A-Telltale-Sign-Of-Summer-Hyd-272112430.jpg",
+    "https://images.unsplash.com/photo-1596626587133-9dd92b699bc6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1473&q=80",
   ]);
+
+  const [newImageUrl, setnewImageUrl] = useState("");
 
   function ShowImage() {
     return Images.map((image) => {
@@ -17,18 +19,14 @@ export default function Images() {
   }
 
   function handleAdd() {
-    // let tempImage = Images;
+    setImages([newImageUrl, ...Images]);
+    setnewImageUrl("");
+  }
 
-    // let tempImage = [...Images];
-    // tempImage.push("https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80")
-    // console.log(tempImage);
-    // setImages(tempImage)
+  function handleChange(event) {
+    // console.log(event.target.value);
 
-    setImages([
-      ...Images,
-      "https://images.unsplash.com/photo-1593642634524-b40b5baae6bb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1489&q=80",
-    ]);
-    // console.log("Working");
+    setnewImageUrl(event.target.value);
   }
 
   return (
@@ -41,6 +39,8 @@ export default function Images() {
         <input
           type="text"
           className="p-2 border border-gray-800 shadow rounded"
+          value={newImageUrl}
+          onChange={handleChange}
         />
         <button className="p-2 bg-green-600 text-white" onClick={handleAdd}>
           Add New
